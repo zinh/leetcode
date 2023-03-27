9d6f27cd1d666d39fd0ad4d81a25cb8ba87cc1f2
Vinh Nguyen <zinh@users.noreply.github.com>
2023-03-27 16:54:47 +0900

    prod 344

---
+++ prod344.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        pass
