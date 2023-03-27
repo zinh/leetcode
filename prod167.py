109cd64438d0c4d40b546a0914afaa59919cfffa
Vinh Nguyen <zinh@users.noreply.github.com>
2023-03-27 16:53:47 +0900

    optimize

--- prod167.py
+++ prod167.py
@@ -33,7 +33,7 @@ class Solution2:
             s = numbers[left] + numbers[right]
             if s == target:
                 return [left +1, right + 1]
-            elif s > target:
+            if s > target:
                 right -= 1
             else:
                 left += 1
