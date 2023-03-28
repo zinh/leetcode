3ac73f7b095082c0fe414f62063165533a579999
Vinh <zinh@users.noreply.github.com>
2023-03-29 08:55:16 +0900

    restructure folder

--- prod19.py
+++ prod19.py
@@ -1,6 +1,14 @@
 from typing import Optional
-from prod876 import ListNode 
+from python.prod876 import ListNode 
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        pass
+        fast = slow = head
+        for i in range(n):
+            fast = fast.next
+            if fast is None:
+                return None
+        return fast
+
+if __name__ == '__main__':
+    head = ListNode.from_list([1,2,3,4,5])
